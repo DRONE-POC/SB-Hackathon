9a263ca6db9b000d1239f89f310ba374564c5c7c
Chris Woodworth <cwoodwor@travelers.com>
2017-07-22 16:06:06 -0400

    simplify code

--- chainbreaker/app.js
+++ chainbreaker/app.js
@@ -14,7 +14,7 @@ var db = require('./utils/db');
 
 passport.use(new Strategy(
   function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
+    db.findByUsername(username, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
@@ -27,7 +27,7 @@ passport.serializeUser(function(user, cb) {
 });
 
 passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
+  db.findById(id, function (err, user) {
     if (err) { return cb(err); }
     cb(null, user);
   });
